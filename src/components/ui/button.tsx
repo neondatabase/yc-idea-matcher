9f6673f24878af1f3163fe9d32420350f2f37e5a
Mahmoud Abdelwahab <hey@mahmoud.codes>
2023-08-03 16:28:10 +0300

    cleanup

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,14 +1,6 @@
 'use client';
 import * as React from 'react';
 import type { HTMLAttributes } from 'react';
-import {
-  motion,
-  useAnimationFrame,
-  useMotionTemplate,
-  useMotionValue,
-  useTransform,
-  AnimatePresence,
-} from 'framer-motion';
 import { cn } from '~/utils';
 
 export interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
@@ -76,118 +68,10 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         )}
       >
         {leadingIcon}
-        <BorderMask width={2}>
-          <AnimatePresence>
-            {loading && (
-              <BorderFollower>
-                <div className="h-20 w-20 opacity-30" />
-              </BorderFollower>
-            )}
-          </AnimatePresence>
-        </BorderMask>
+
         {loading ? 'Loading...' : children}
         {trailingIcon}
       </button>
     );
   }
 );
-
-interface BorderMaskProps {
-  width: string | number;
-  radius?: string | number;
-  children?: React.ReactNode;
-}
-
-const BorderMask = ({
-  width,
-  radius = 'inherit',
-  children,
-  ...otherProps
-}: BorderMaskProps) => {
-  const widthWithUnit = typeof width === 'number' ? width + 'px' : width;
-  return (
-    <div
-      style={{
-        inset: `calc(-1 * ${widthWithUnit})`,
-        borderWidth: width,
-        borderRadius: 'inherit',
-        WebkitMask:
-          'linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)',
-        WebkitMaskComposite: 'xor',
-        maskComposite: 'exclude',
-      }}
-      className={cn('pointer-events-none absolute  border-transparent')}
-      {...otherProps}
-    >
-      <div
-        className="absolute overflow-hidden"
-        style={{
-          inset: `calc(-1 * ${widthWithUnit})`,
-        }}
-      >
-        {children}
-      </div>
-    </div>
-  );
-};
-
-const BorderFollower = ({
-  children,
-  duration = 2000,
-  radius,
-  ...otherProps
-}: any) => {
-  const pathRef = React.useRef<SVGRectElement>();
-  const progress = useMotionValue<number>(0);
-
-  useAnimationFrame((time) => {
-    const length = pathRef.current?.getTotalLength();
-    if (length) {
-      const pxPerMillisecond = length / duration;
-      progress.set((time * pxPerMillisecond) % length);
-    }
-  });
-
-  const x = useTransform(
-    progress,
-    (val) => pathRef.current?.getPointAtLength(val).x
-  );
-  const y = useTransform(
-    progress,
-    (val) => pathRef.current?.getPointAtLength(val).y
-  );
-
-  const transform = useMotionTemplate`translateX(${x}px) translateY(${y}px) translateX(-50%) translateY(-50%)`;
-
-  return (
-    <svg
-      className="absolute h-full w-full"
-      xmlns="http://www.w3.org/2000/svg"
-      preserveAspectRatio="none"
-      {...otherProps}
-    >
-      <rect
-        fill="none"
-        className={`rounded-[${radius}]`}
-        width="100%"
-        height="100%"
-        // @ts-ignore
-        ref={pathRef}
-      />
-      <foreignObject x="0" y="0" width="100%" height="100%">
-        <motion.div
-          className="bg-radial-gradient"
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            display: 'inline-block',
-            transform,
-          }}
-        >
-          {children}
-        </motion.div>
-      </foreignObject>
-    </svg>
-  );
-};
