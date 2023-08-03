8acba622045f1bf51d27a9560678ef9eaff5ef93
Mahmoud Abdelwahab <hey@mahmoud.codes>
2023-08-03 16:22:02 +0300

    add Vercel Analytics

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '~/lib/query-client';
+import { Analytics } from '@vercel/analytics/react';
 
 type Props = {
   children: React.ReactNode;
@@ -8,6 +9,9 @@ type Props = {
 
 export default function Providers({ children }: Props) {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <Analytics />
+    </QueryClientProvider>
   );
 }
