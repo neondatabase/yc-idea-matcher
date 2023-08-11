17afb0492d0751ae55c8876146af21bc101d8089
Mahmoud Abdelwahab <hey@mahmoud.codes>
2023-08-11 11:50:41 +0300

    add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Link from 'next/link';
 import Providers from '~/components/providers';
 import { cn } from '~/utils';
 
@@ -21,12 +22,36 @@ export default function RootLayout({
     <html
       lang="en"
       className={cn(
-        'bg-[#080808] text-gray-1100 selection:text-gray-1200 selection:bg-green-800',
+        'bg-[#080808] text-gray-1100 selection:text-gray-1200 selection:bg-green-800 flex flex-col min-h-[95vh]',
         inter.className
       )}
     >
       <Providers>
-        <body>{children}</body>
+        <body className="flex-grow">{children}</body>
+        <footer className="my-5">
+          <p className="text-center text-sm">
+            Powered by{' '}
+            <a
+              className="focus-visible:border-gray-700 focus-visible:ring-2 focus-visible:ring-green-700 focus-visible:ring-offset-2 focus-visible:ring-offset-gray-100
+            transition-colors outline-none rounded-md text-gray-1200 hover:underline"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://bit.ly/3OFaSTp"
+            >
+              Neon
+            </a>{' '}
+            and{' '}
+            <a
+              className="focus-visible:border-gray-700 focus-visible:ring-2 focus-visible:ring-green-700 focus-visible:ring-offset-2 focus-visible:ring-offset-gray-100
+            transition-colors outline-none rounded-md text-gray-1200 hover:underline"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/neondatabase/yc-idea-matcher"
+            >
+              pg_embedding
+            </a>
+          </p>
+        </footer>
       </Providers>
     </html>
   );
